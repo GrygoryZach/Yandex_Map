fe630e3e569bc7726f40fa9f636b058d89954b35
GrygoryZach <grishazschepilo@gmail.com>
2024-02-03 19:49:58 +0100

    2 задание

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygame import init as pginit
 from pygame import display
 from pygame import event as pgevent
-from pygame import QUIT, KEYDOWN, K_PAGEUP, K_PAGEDOWN
+from pygame import QUIT, KEYDOWN, K_PAGEUP, K_PAGEDOWN, K_UP, K_DOWN, K_LEFT, K_RIGHT
 from pygame import sprite
 
 from map_view import MapView
@@ -9,7 +9,7 @@ from map_view import MapView
 
 def main() -> None:
     pginit()
-    
+
     all_sprites = sprite.Group()
 
     size = 600, 450
@@ -28,12 +28,18 @@ def main() -> None:
                     map_view.scale("inc")
                 if event.key == K_PAGEDOWN:
                     map_view.scale("dec")
-        
+                if event.key == K_UP:
+                    map_view.move("up")
+                if event.key == K_DOWN:
+                    map_view.move("down")
+                if event.key == K_LEFT:
+                    map_view.move("left")
+                if event.key == K_RIGHT:
+                    map_view.move("right")
+
         all_sprites.draw(screen)
         display.flip()
 
 
 if __name__ == "__main__":
     main()
-
-
--- map_view.py
+++ map_view.py
@@ -6,7 +6,8 @@ from io import BytesIO
 
 
 class MapView(sprite.Sprite):
-    def __init__(self, init_coords: Tuple[float, float] = (37.677751, 55.757718), init_zoom: int = 9, *groups: sprite.Group) -> None:
+    def __init__(self, init_coords: Tuple[float, float] = (37.677751, 55.757718), init_zoom: int = 4,
+                 *groups: sprite.Group) -> None:
         super().__init__(*groups)
         self.yx = init_coords
         self.zoom = init_zoom
@@ -16,23 +17,28 @@ class MapView(sprite.Sprite):
             "z": str(self.zoom),
             "l": "map",
         }
+        self.update()
 
-        response = get(self.static_api_server, params=self.static_api_params)
-        self.image = image.load(BytesIO(response.content))
-        self.rect = self.image.get_rect()
-    
     def move(self, direction: Literal["up", "down", "left", "right"]) -> None:
         new_y, new_x = self.yx
+        change = 360 / (2 ** self.zoom)
         match direction:
             case "up":
-                new_y += 10
+                if new_x + change < 100:
+                    new_x += change
             case "down":
-                new_y -= 10
+                if new_x - change > -100:
+                    new_x -= change
             case "left":
-                new_x -= 10
+                new_y -= change
             case "right":
-                new_x += 1
-    
+                new_y += change
+
+        self.yx = new_y, new_x
+        print(self.yx)
+        self.static_api_params["ll"] = "{0},{1}".format(*self.yx)
+        self.update()
+
     def scale(self, scale: Literal["inc", "dec"]) -> None:
         match scale:
             case "inc":
@@ -42,7 +48,9 @@ class MapView(sprite.Sprite):
                 if 0 <= self.zoom - 1 <= 21:
                     self.zoom -= 1
         self.static_api_params["z"] = str(self.zoom)
+        self.update()
+
+    def update(self) -> None:
         response = get(self.static_api_server, params=self.static_api_params)
         self.image = image.load(BytesIO(response.content))
         self.rect = self.image.get_rect()
-
